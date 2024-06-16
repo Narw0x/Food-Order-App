e978019b17135b1525f6ac93d71f09fe5a2a330f
Martin Synák <martin.synak123@gmail.com>
2024-06-16 20:32:26 +0200

    creating cart context and updating it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,15 @@
+import { useContext } from 'react';
 import logo from '../assets/logo.jpg';
 
+import Button from './UI/Button';
+
+import CartContext from '../store/CartContext';
+
 export default function Header() {
+
+    const CartCtx = useContext(CartContext);
+
+    const totalCartItems = CartCtx.items.reduce((acc, item) => acc + item.quantity, 0);
 
     return(
         <header id="main-header">
@@ -9,7 +18,7 @@ export default function Header() {
                 <h1>React Food </h1>
             </div>
            <nav>
-            <button>Cart (0)</button>
+            <Button textOnly >Cart ({totalCartItems})</Button>
            </nav>
         </header>
     )
--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,15 +1,29 @@
+import { useContext } from "react"
+import { currencyFormatter } from "../util/formatting"
+
+import CartContext from "../store/CartContext";
+
+import Button from "./UI/Button"
+
 export default function MealItem({ meal }) {
+    const cartCtx = useContext(CartContext);
+
+    function addMealToCart(){
+        cartCtx.addToCart(meal)
+    }
+
+
     return(
         <li className="meal-item" >
             <article>
                 <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
                 <div>
                     <h3>{meal.name}</h3>
-                    <p className="meal-item-price">{meal.price}</p>
+                    <p className="meal-item-price">{currencyFormatter.format(meal.price)}</p>
                     <p className="meal-item-description">{meal.description}</p>
                 </div>
                 <p className="meal-item-actions">
-                    <button>Add to cart</button>
+                    <Button onClick={addMealToCart}>Add to Cart</Button>
                 </p>
             </article>
             
